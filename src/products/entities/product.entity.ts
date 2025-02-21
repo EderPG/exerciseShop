38f21236006ca3d4c43444e84251532999c25ae0
EderPG <l20120144@morelia.tecnm.mx>
2025-02-21 17:54:48 -0600

    fixed products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -13,9 +13,7 @@ export class tblProducts {
   })
   Product_strUniqueKey: string;
 
-  @Column('text', {
-    nullable: true,
-  })
+  @Column('text')
   Product_strDescription: string;
 
   @Column('float', {
